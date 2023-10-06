ab7566afee533eb1429e34610742ac6ddc42936a
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-10-05 23:14:08 -0500

    test for includes method has passed

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -23,7 +23,21 @@ class LinkedList:
 
     def includes(self, value):
         # method body here
-        if self.head.value:
+        # if self.head.value:
+        #     print(self.head)
+        #     print(self.head.value)
+        #     return True
+        # else:
+        #     return False
+        current = self.head
+
+        linked_list_values = []
+
+        while current:
+            linked_list_values.append(current.value)
+            current = current.next
+
+        if value in linked_list_values:
             return True
         else:
             return False
@@ -31,33 +45,18 @@ class LinkedList:
     def __str__(self):
         # method body here
         current = self.head
-        # print(current) # shows a memory address, <data_structures.linked_list.Node object at 0x10505dc10>
-        # print(current.value) # shows apple
-        # print(current.next) # shows None
 
         if current is None:
             return "NULL"
 
-        linked_list_values = []
+        linked_list_values = ""
 
         while current:
-            # (f"{{ {current.value} }} -> NULL") # shows { apple } -> NULL
             # performs logic
-            linked_list_values.append(current.value)
+            linked_list_values += f"{{ {current.value} }} -> "
             current = current.next
 
-        # need this to be more dynamic, can't figure it out atm
-        if(len(linked_list_values) == 1):
-            return(f"{{ {linked_list_values[0]} }} -> NULL")
-        if(len(linked_list_values) == 2):
-            return(f"{{ {linked_list_values[0]} }} -> {{ {linked_list_values[1]} }} -> NULL")
-
-        # for value in linked_list_values:
-        #     return f"{{ {value} }}"
-
-
-
-
+        return linked_list_values + 'NULL'
 
 
 class TargetError:
