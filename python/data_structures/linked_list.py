26c69bccf0495eba6e61e383d662c1e462c7fdd3
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-10-09 23:04:09 -0500

    all tests are passing

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -4,18 +4,11 @@ class Node:
         self.next = next
 
 class LinkedList:
-    """
-    Put docstring here
-    """
-
     def __init__(self, head=None):
         # initialization here
         self.head = head
 
     def insert(self, value):
-        """
-
-        """
         if self.head is None:
             self.head = Node(value)
         else:
@@ -27,57 +20,37 @@ class LinkedList:
         """
         Adds a new node with the given new value immediately before the first node that has the value specified
         """
-        # my code that didn't work
-        # again have to start at head node
-        # current = self.head
-        #
-        # new_node = Node(value)
-
-        # find node with matching value
-        # point new node to the node to insert before
-        # point head node to new node
-        # while current.next:
-        #     if current.value == value:
-        #         new_node.next = current
-        #         current = current.next
-        # self.head.next = new_node
-
         new_node = Node(new_value)  # Create a new node with the new_value
 
-        # Figured out lines 48 - 55 all by myself. YAY!
         if self.head is None:
-            self.head = new_node
-            return
+            raise TargetError
 
         current = self.head
-        if current.value == value:
-            new_node.next = current
-            self.head = new_node
 
         #with ChatGPT's help
         while current.next:
             if current.next.value == value:  # Check the next node's value
                 new_node.next = current.next  # Set the next of the new node to the next of the current node
                 current.next = new_node  # Update the current node's next to the new node
-                break  # Exit the loop after insertion
+                return  # Exit the loop after insertion
             current = current.next  # Move to the next node in the list
+
+        if current.value == value:
+            new_node.next = current
+            self.head = new_node
+        else:
+            raise TargetError
+
 
     def insert_after(self, value, new_value):
         """
         Adds a new node with the given new value immediately after the first node that has the value specified
         """
+        if self.head is None:
+            raise TargetError
 
         current = self.head
-
         new_node = Node(new_value)
-
-        # my non-working code
-        # while current.next:
-        #     if current.next.value == value:
-        #         old_next = current.next
-        #         current.next = new_node
-        #         new_node.next = old_next
-        #         break
 
         # per ChatGPT recommendations
         while current:
@@ -85,20 +58,14 @@ class LinkedList:
                 old_next = current.next
                 current.next = new_node
                 new_node.next = old_next
-                break
+                return
             current = current.next  # Move to the next node
+        raise TargetError
 
     def append(self, value):
         """
         Adds a new node with the given value to the end of the list
         """
-        # my attempt
-        # current = self.head
-        # while current:
-        #     current = current.next
-        # # new_node = Node(value)
-        # current.next = Node(value)
-
         #with help from ChatGPT
         ## step one - instantiate new node
         new_node = Node(value)
@@ -120,9 +87,6 @@ class LinkedList:
 
 
     def includes(self, value):
-        """
-
-        """
         current = self.head
 
         linked_list_values = []
@@ -153,6 +117,6 @@ class LinkedList:
         return linked_list_values + 'NULL'
 
 
-
 class TargetError(Exception):
-    raise Exception('TargetError')
+    pass
+
