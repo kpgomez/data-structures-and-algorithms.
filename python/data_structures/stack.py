8c98130b244a8cabd2be180d01dcf19164c60046
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-10-17 23:06:37 -0500

    initial file structure; first test for stack passes

--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,12 +1,34 @@
-class Stack:
+class Node:
     """
-    Put docstring here
+    A Node class that knows about its value and next
     """
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
 
+
+class Stack():
+    """
+    A Stack class consists of Nodes that knows only about their top
+    """
     def __init__(self):
         # initialization here
         pass
 
-    def some_method(self):
+    def push(self, value: str):
+        """
+        Instance method that adds input value to current stack
+        :param value: takes a string as an input representing the value to be pushed onto the stack
+        :return: no return
+        """
         # method body here
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def is_empty(self):
         pass
