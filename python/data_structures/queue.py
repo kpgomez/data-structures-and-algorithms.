c689b5f7fd029485d40249bfb857ac989b76f76b
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-10-19 00:21:02 -0500

    first three tests for queues are passing

--- python/data_structures/queue.py
+++ python/data_structures/queue.py
@@ -1,12 +1,66 @@
+from data_structures.invalid_operation_error import InvalidOperationError
+
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
 class Queue:
     """
-    Put docstring here
+    A Queue consists of Nodes. It knows about its front and rear.
     """
 
-    def __init__(self):
-        # initialization here
+    def __init__(self, back=None):
+        self.front = back
+        self.back = back
+
+    def enqueue(self, value: str):
+        """
+        Adds a new node containing value to the back of the queue with an O(1) Time complexity
+        :param value:
+        :return: None
+        """
+        # step one: create new node
+        # new_node = Node(value)
+        # # step two: point new_node to same as back
+        # new_node.next = self.back
+        # # step three: point back to new node
+        # self.back = new_node
+
+        # if queue is empty, lines
+        if self.front is None:
+            new_node = Node(value)
+            self.front = new_node
+            self.back = new_node
+
+        else:
+            new_node = Node(value)
+            new_node = self.back
+            self.back = new_node
+
+
+    def dequeue(self) -> str:
+        """
+        Removes the node from the front of the queue
+        :return: string representing the value of the node removed
+        """
+        # raise Exception when empty
+        if self.front is None:
+            raise InvalidOperationError
+
+        # step one: create a ref that points to front
+        old_front = self.front
+        # step two: point front to front.next
+        new_front = self.front.next
+        # step three: point new front to None
+        # new_front.next = None
+        # # return value of old_front
+        return old_front.value
+
+    def peek(self):
         pass
 
-    def some_method(self):
-        # method body here
+    def is_empty(self):
         pass
--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -14,14 +14,14 @@ class Stack:
     """
     A Stack class is made up of Nodes. It only knows about its top
     """
-    def __init__(self):
-        self.top = None
+    def __init__(self, top: Node=None):
+        self.top = top
 
     def push(self, value: str):
         """
         An instance method of class Stack that adds the input value to current stack
-        :param value: takes a string as an input representing the value to be pushed onto the stack
-        :return: there is no return
+        :param value: takes in a string that represents the value to be pushed onto the stack
+        :return: None
         """
         # step one: create a new node
         new_node = Node(value)
@@ -30,10 +30,10 @@ class Stack:
         # step three: point top to new_node ** pointing goes in this direction -> **
         self.top = new_node
 
-    def pop(self):
+    def pop(self) -> str:
         """
         An instance method of class Stack that removes Node from the top of the current stack and reassigns top the previous node's next
-        :return: the string value of the Node that was removed
+        :return: the string representing the value of the Node that was removed
         """
         # raise exception when empty
         if self.top is None:
@@ -47,11 +47,13 @@ class Stack:
         old_node.next = None
         return old_node.value
 
-    def peek(self):
+    def peek(self) -> str:
         if self.top is None:
             raise InvalidOperationError("Method not allowed on empty collection")
         return self.top.value
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         if self.top is None:
             return True
+        # A more pythonic syntax per Jacob's recommendation
+        # return self.top is None
