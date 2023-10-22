cbacfb241ad9ae538dbcb18aba4b8eef2c40deaa
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-10-22 15:54:51 -0500

    basic file structure

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -1,5 +1,30 @@
 from data_structures.stack import Stack
 
 
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
 class PseudoQueue:
-    pass
+    def __init__(self, front=None, rear=None):
+        self.front = rear # when there is a single node, both front and rear point to same node
+        self.rear = rear
+
+    def enqueue(self, value: str) -> None:
+        """
+        Inserts a value into the PseudoQueue, using a FIFO approach
+        :param value: a string representing value to be added to PseudoQueue
+        :return: None
+        """
+        pass
+
+    def dequeue(self) -> str:
+        """
+        Extracts a value from the PseudoQueue, using a FIFO approach
+        :return: a string representing the value removed from the PseudoQueue
+        """
+        pass
+
+
