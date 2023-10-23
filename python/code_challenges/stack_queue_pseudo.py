37020628f566de6ab6f42ea0f5d525e072d2af5e
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-10-23 18:31:47 -0500

    all tests passing

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -1,30 +1,26 @@
 from data_structures.stack import Stack
 
 
-class Node:
-    def __init__(self, value, next=None):
-        self.value = value
-        self.next = next
-
-
+# https://chat.openai.com/c/2f646257-a994-49ef-ac80-4a4d6b5cf60e
 class PseudoQueue:
-    def __init__(self, front=None, rear=None):
-        self.front = rear # when there is a single node, both front and rear point to same node
-        self.rear = rear
+    def __init__(self):
+        # create two instances of a Stack, one for enqueueing and one for dequeueing
+        self.in_stack = Stack()
+        self.out_stack = Stack()
 
-    def enqueue(self, value: str) -> None:
-        """
-        Inserts a value into the PseudoQueue, using a FIFO approach
-        :param value: a string representing value to be added to PseudoQueue
-        :return: None
-        """
-        pass
+    def enqueue(self, value):
+        # push new value into the in_stack
+        self.in_stack.push(value)
 
-    def dequeue(self) -> str:
-        """
-        Extracts a value from the PseudoQueue, using a FIFO approach
-        :return: a string representing the value removed from the PseudoQueue
-        """
-        pass
-
-
+    def dequeue(self):
+        # first check if it is the top
+        if not self.out_stack.top:
+            # while truthy or top is not None so basically the entire length of the in_stack
+            while self.in_stack.top:
+                # first pops the top value of in_stack then push that value into the out_stack,
+                # this essentially reverses the order of the stack from newest value on top to
+                # now oldest value on top
+                self.out_stack.push(self.in_stack.pop())
+        # once the stack out_stack reverses the order of what was once the in_stack,
+        # return the top value of the out_stack
+        return self.out_stack.pop()
