2c6b53bdb40b234ec9230e559c27c03b8111dc53
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-11-02 19:24:37 -0500

    removed nested ifs

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -2,39 +2,33 @@ from data_structures.queue import Queue
 
 
 def multi_bracket_validation(expr: str) -> bool:
-    open_brackets = ('(', '{', '[')
-    close_brackets = (')', '}', ']')
-
     round_brackets = Queue()
     curly_brackets = Queue()
     square_brackets = Queue()
-
     empty = []
 
     for char in expr:
-        if char in open_brackets:
-            if char == '(':
-                round_brackets.enqueue(char)
-            if char == '{':
-                curly_brackets.enqueue(char)
-            if char == '[':
-                square_brackets.enqueue(char)
-        if char in close_brackets:
-            if char == ')':
-                if round_brackets.is_empty():
-                    return False
-                else:
-                    round_brackets.dequeue()
-            if char == '}':
-                if curly_brackets.is_empty():
-                    return False
-                else:
-                    curly_brackets.dequeue()
-            if char == ']':
-                if square_brackets.is_empty():
-                    return False
-                else:
-                    square_brackets.dequeue()
+        if char == '(':
+            round_brackets.enqueue(char)
+        if char == '{':
+            curly_brackets.enqueue(char)
+        if char == '[':
+            square_brackets.enqueue(char)
+        if char == ')':
+            if round_brackets.is_empty():
+                return False
+            else:
+                round_brackets.dequeue()
+        if char == '}':
+            if curly_brackets.is_empty():
+                return False
+            else:
+                curly_brackets.dequeue()
+        if char == ']':
+            if square_brackets.is_empty():
+                return False
+            else:
+                square_brackets.dequeue()
 
     # confirm all queues are empty
     empty.append(round_brackets.is_empty())
