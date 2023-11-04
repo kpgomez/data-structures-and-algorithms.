61a127c161a306dc949be33526328768518a3e86
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-11-04 18:44:46 -0500

    all pre-defined binary tree tests have passed

--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -1,16 +1,87 @@
+class Node:
+    """
+    A Node class knows its value, left, and right child node.
+    """
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
 class BinaryTree:
     """
     Put docstring here
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.root = None
 
-    def some_method(self):
-        # method body here
-        pass
+    # https://www.udemy.com/course/data-structures-algorithms-python/
+    def pre_order(self) -> list:
+        """
+        This is a depth first traversal and the order of traversal is root, left, right
+        :return: list a values
+        """
+        results = []
 
+        def traverse(current: Node):
+            if current is None:
+                return
 
-class Node:
-    pass
+            results.append(current.value)
+
+            if current.left is not None:
+                traverse(current.left)
+
+            if current.right is not None:
+                traverse(current.right)
+
+        traverse(self.root)
+
+        return results
+
+    def in_order(self) -> list:
+        """
+        This is a depth first traversal and the order of traversal is left, root, right
+        :return:
+        """
+        results = []
+
+        def traverse(current: Node):
+            if current is None:
+                return
+
+            if current.left is not None:
+                traverse(current.left)
+
+            results.append(current.value)
+
+            if current.right is not None:
+                traverse(current.right)
+
+        traverse(self.root)
+
+        return results
+
+    def post_order(self) -> list:
+
+        """
+        This is a depth first traversal and the order of traversal is left, right, root
+        :return:
+        """
+        results = []
+
+        def traverse(current: Node):
+            if current is None:
+                return
+
+            if current.left is not None:
+                traverse(current.left)
+
+            if current.right is not None:
+                traverse(current.right)
+
+            results.append(current.value)
+
+        traverse(self.root)
+
+        return results
