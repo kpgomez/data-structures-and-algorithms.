24a7ec4de043ecaba2857813b92c89fc80bfc3fe
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-11-13 22:40:48 -0600

    all pre-defined tests have passed

--- python/code_challenges/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first.py
@@ -1,5 +1,19 @@
 from data_structures.binary_tree import BinaryTree
 
 
-def breadth_first():
-    pass
+def breadth_first(tree):
+    # initialize empty list
+    tree_nodes = []  # will contain list of node values
+    if tree.root is None:
+        return tree_nodes
+    tree_queue = [tree.root]  # will hold list of nodes
+    # tree_queue.append(tree.root)
+    while tree_queue:
+        current_node = tree_queue[0]
+        tree_nodes.append(current_node.value)
+        tree_queue.pop(0)
+        if current_node.left:
+            tree_queue.append(current_node.left)
+        if current_node.right:
+            tree_queue.append(current_node.right)
+    return tree_nodes
