cfc5c88d3a1fb6247121c8ef0b3535448629e95a
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-11-16 23:38:04 -0600

    not passing at this time

--- python/code_challenges/tree_fizz_buzz.py
+++ python/code_challenges/tree_fizz_buzz.py
@@ -1,5 +1,27 @@
 from data_structures.binary_tree import BinaryTree
+from data_structures.kary_tree import KaryTree, Node
 
 
-def fizz_buzz_tree():
-    pass
+def fizz_buzz_tree(root):
+    k_ary_tree = KaryTree(Node(root))
+    print(k_ary_tree.breadth_first())
+    node_values = k_ary_tree.breadth_first()
+    print(node_values)
+    fb_tree = []
+    for node in node_values:
+        result = check_fizz_buzz(node)
+        fb_tree.append(result)
+    return fb_tree
+
+
+def check_fizz_buzz(node):
+    if node % 5 == 0 and node % 3 == 0:
+        node.value = "FizzBuzz"
+    elif node.value % 5 == 0:
+        node.value = "Buzz"
+    elif node.value % 3 == 0:
+        node.value = "Fizz"
+    else:
+        old_value = node.value
+        node.value = str(old_value)
+    return node.value
