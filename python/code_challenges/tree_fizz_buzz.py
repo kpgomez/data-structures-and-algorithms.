99e54728ca648e8752f115bbf0b891e3d4286075
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-12-20 01:57:18 -0600

    getting closer to solution

--- python/code_challenges/tree_fizz_buzz.py
+++ python/code_challenges/tree_fizz_buzz.py
@@ -3,14 +3,14 @@
 from data_structures.queue import Queue
 
 
-class KaryNode:
+class Node:
     def __init__(self, value):
         self.value = value
         self.children = []
 
 
 class KaryTree:
-    def __init__(self, root: KaryNode = None):
+    def __init__(self, root: Node = None):
         self.root = root
 
     def breadth_first(self):
@@ -19,29 +19,155 @@ class KaryTree:
         queue.enqueue(self.root)
         collection = []
 
+        # while queue is not empty
         while not queue.is_empty():
             node = queue.dequeue()
+            # print('line 25', node.value, node.children)
             collection.append(str(node.value))
 
             for child in node.children:
+                # print('child line 29', child.value)
                 queue.enqueue(child)
         # print('collection', collection)
         return collection
 
-    def copy(self):
-        copy = self.breadth_first() # this is a list
-        return copy
-
 
 def check_fizz_buzz(node):
-    if int(node) % 15 == 0:
+    # print('fizz buzz node line 40', node)
+    # print('node.value line 41', node.value)
+    if node.value % 15 == 0:
         return "fizzbuzz"
-    elif int(node) % 5 == 0:
+    elif node.value % 5 == 0:
         return "fizz"
-    elif int(node) % 3 == 0:
+    elif node.value % 3 == 0:
         return "buzz"
     else:
-        return node
+        return str(node.value)
+
+
+def fizz_buzz_tree(k_ary_tree: KaryTree) -> Node:
+    # maintain ref to old root
+    before_fizz_root = k_ary_tree.root
+
+    # check fizzbuzz on  root node
+    fizzed_root = Node(check_fizz_buzz(before_fizz_root))
+    # print(fizzed_root)
+    # print(fizzed_root.value)
+
+    # # create new fizzed tree
+    new_tree = KaryTree(fizzed_root)
+    #
+    # use queue to traverse old tree
+    queue = Queue()
+    queue.enqueue(before_fizz_root)
+
+    current = fizzed_root
+    #
+    while not queue.is_empty():
+        collection = []
+        node = queue.dequeue()
+        collection.append(node)
+        for child in node.children:
+            queue.enqueue(child)
+            current.children = [Node(check_fizz_buzz(child)) for item in collection]
+
+    return new_tree
+
+
+if __name__ == "__main__":
+    root = Node(1)
+    first_child = Node(2)
+    second_child = Node(3)
+    third_child = Node(4)
+    fourth_child = Node(5)
+    grandchild_one = Node(10)
+    grandchild_two = Node(15)
+    fourth_child.children = [grandchild_one, grandchild_two]
+    root.children = [first_child, second_child, third_child, fourth_child]
+    tree = KaryTree(root)
+    # print('tree line 109', tree)
+    print('tree breadth first line 110', tree.breadth_first())
+    fizzy_tree = fizz_buzz_tree(tree)
+    print('fizz_tree.bfs line 112', fizzy_tree.breadth_first())
+    # print('tree line 66', tree)
+    # print('root', root)
+    # print('root.value', root.value)
+    # print('root.children', root.children)
+    # for child in root.children:
+    #     print('child.value', child.value)
+    # actual = tree.breadth_first()
+    # print('actual line 58', actual)
+    # new = tree.copy()
+    # print('new line 61', new)
+    # fizz_buzz_tree_input_node(tree)
+    # fizzy_tree = fizz_buzz_tree(tree)
+    # copied_tree = fizzy_tree.copy()
+
+
+
+# def fizz_buzz_tree(k_ary_tree: KaryTree) -> Node:
+#     # print('k_ary_tree', k_ary_tree)
+#     # copy_tree = KaryTree(k_ary_tree.root).copy()
+#     # print('copy_tree', copy_tree)
+#     copy_tree = KaryTree(k_ary_tree.root)
+#
+#     # traverse the copy and build new tree
+#     current = copy_tree.root
+#     print('current line 69', current.value)
+#     new_tree_queue = Queue()
+#     new_tree_queue.enqueue(current)
+#     fizz_check = []
+#     #
+#     # while not new_tree_queue.is_empty():
+#     #     new_node = new_tree_queue.dequeue()
+#     #     fizz_check.append(new_node)
+#
+#
+#     # print('copy_tree', copy_tree)
+#     # clone = []
+#     # for node in copy_tree.copy():
+#     #     result = check_fizz_buzz(node)
+#     #     clone.append(result)
+#     # print('clone', clone)
+#     # fizz_tree = KaryTree(KaryNode(clone[0]))
+#     return copy_tree
+#     # return clone
+
+
+# def fizz_buzz_tree(k_ary_tree: KaryTree) -> Node:
+#     new_tree = KaryTree(check_fizz_buzz(k_ary_tree.root))
+#     # print('new', new_tree)
+#     # print('new value line 67', new_tree.root)
+#     # result = check_fizz_buzz(k_ary_tree.root)
+#     # print('result', result)
+#     # print('type of result', type(result))
+#     # k_ary_tree.root.value = result
+#     # new_tree = KaryTree(k_ary_tree.root)
+#     # print('new tree line 73', new_tree)
+#
+#     # traverse the copy and build new tree
+#     # current = new_tree.root
+#     # print('current line 77', current.value)
+#     # new_tree_queue = Queue()
+#     # new_tree_queue.enqueue(current)
+#     fizz_check = []
+#     #
+#     # while not new_tree_queue.is_empty():
+#     #     new_node = new_tree_queue.dequeue()
+#     #     fizz_check.append(new_node)
+#
+#
+#     # print('copy_tree', copy_tree)
+#     # clone = []
+#     # for node in copy_tree.copy():
+#     #     result = check_fizz_buzz(node)
+#     #     clone.append(result)
+#     # print('clone', clone)
+#     # fizz_tree = KaryTree(KaryNode(clone[0]))
+#     print('new tree', new_tree)
+#     return new_tree
+#     # return clone
+
 
 
 # def fizz_buzz_tree_input_node(k_ary_tree_root: KaryNode) -> KaryNode:
@@ -56,52 +182,6 @@ def check_fizz_buzz(node):
 #     return clone
 
 
-def fizz_buzz_tree(k_ary_tree: KaryTree) -> KaryNode:
-    # print('k_ary_tree', k_ary_tree)
-    # copy_tree = KaryTree(k_ary_tree.root).copy()
-    # print('copy_tree', copy_tree)
-    copy_tree = KaryTree(k_ary_tree.root)
-
-    # traverse the copy and build new tree
-    current = copy_tree.root
-    print('current', current)
-    new_tree_queue = Queue()
-    new_tree_queue.enqueue(current)
-    fizz_check = []
-    #
-    # while not new_tree_queue.is_empty():
-    #     new_node = new_tree_queue.dequeue()
-    #     fizz_check.append(new_node)
-
-
-    # print('copy_tree', copy_tree)
-    # clone = []
-    # for node in copy_tree.copy():
-    #     result = check_fizz_buzz(node)
-    #     clone.append(result)
-    # print('clone', clone)
-    # fizz_tree = KaryTree(KaryNode(clone[0]))
-    return copy_tree
-    # return clone
-
-
-if __name__ == "__main__":
-    root = KaryNode(1)
-    first_child = KaryNode(2)
-    second_child = KaryNode(3)
-    root.children = [first_child, second_child]
-    tree = KaryTree(root)
-    # print('tree line 66', tree)
-    # print('root', root)
-    # print('root.value', root.value)
-    # print('root.children', root.children)
-    # for child in root.children:
-    #     print('child.value', child.value)
-    actual = tree.breadth_first()
-    # print('actual line 58', actual)
-    new = tree.copy()
-    # print('new line 61', new)
-    # fizz_buzz_tree_input_node(tree)
-    fizzy_tree = fizz_buzz_tree(tree)
-    copied_tree = fizzy_tree.copy()
-
+# def copy(self):
+#     copy = self.breadth_first() # this is a list
+#     return copy
